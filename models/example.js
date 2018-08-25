9203c21ecad88070c04522ebed7e96cd77b7c52b
Michaela White <whitemic@sas.upenn.edu>
2018-08-25 13:50:52 -0400

    Merged with master

--- models/example.js
+++ models/example.js
@@ -1,12 +1,4 @@
 module.exports = function(sequelize, DataTypes) {
-  var Questions = sequelize.define("Question", {
-    question: DataTypes.STRING,
-    difficulty: DataTypes.STRING,
-    correctAnswer: DataTypes.STRING,
-    allAnswers: DataTypes.ARRAY
-  });
-  return Questions;
-
       var User = sequelize.define("User", {
       username: DataTypes.STRING,
       password: DataTypes.STRING,
