a3dfe09e9b9c29cdc65c8da12e5660add2b54a45
Guillermo Barila <gui365@yahoo.com.ar>
2018-08-29 15:00:07 -0400

    Added modal to dashboard html to display Session Code

--- public/js/index.js
+++ public/js/index.js
@@ -1,114 +1,93 @@
-// SIGNUP.HTML
-$("#signup-button").on("click", function(event) {
-  event.preventDefault();
-  if (
-    $("#form-username")
-      .val()
-      .trim() === "Guille"
-  ) {
-    $("small").toggleClass("display-none");
-    $("small").toggleClass("display-block");
-  } else {
-    alert("hi");
+//Initialize Firebase
+//hide these keys in .dotenv file
+//link to add to relevant files
+//<script src="https://www.gstatic.com/firebasejs/5.4.1/firebase.js"></script>
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAVctAeThQ7WFsUui0Zdmf_2Mrr-8CoiNg",
+  authDomain: "chooseyourpredator.firebaseapp.com",
+  databaseURL: "https://chooseyourpredator.firebaseio.com",
+  projectId: "chooseyourpredator",
+  storageBucket: "chooseyourpredator.appspot.com",
+  messagingSenderId: "921283071395"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+
+//global variables
+var sessionCode;
+
+var game = {
+  players: [],
+  question: 0
+};
+//generates random code which creates a session and allows other users to join the session
+function generateCode() {
+  var sessionCode = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+  for (var i = 0; i < 6; i++) {
+    sessionCode += characters.charAt(Math.floor(Math.random() * characters.length));
   }
+  // console.log(sessionCode);
+  return sessionCode;
+}
+
+//establish variables for player values to be added to firebase
+// var pID= "";
+// var pOneActive = 0;
+// var pTwoActive = 0;
+// var pThreeActive = 0;
+
+
+
+//when create button is clicked, a code is generated
+//and the session is created in firebase
+$(document).ready(function(){
+  $(".create-button").click(function(){
+    event.preventDefault();
+    sessionCode = generateCode();
+    $(".session-code").text(sessionCode);
+    database.ref("/" + sessionCode).set(game);
+  });
 });
 
-// // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
+//Once three users have input the same code, the session begins
+//Create click event for the #join-button, which presumably will begin once 3 users have entered their codes
+// $("#join-button").on("click", function(event){
+//   //prevents page from refreshing
 //   event.preventDefault();
+//   var isConnected;
+//   if(nameInput === 0) {
 
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
 //   }
 
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
 
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
 
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
 
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
 
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
+// //these brackets close the join-button click event
+// });
+//create code that counts the connections
+//once there are 3 connections, that will trigger the change in HTML
+
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+// var connectionsRef = database.ref("/connections")
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+// var connectedRef = database.ref(".info/connected");
+
+//Will need to cap the amount of connections at 3
+
+//create code that captures each user's answers to questions (form)
+//create code that captures each user's wins/losses
+
+
+
+//once the user logs out of the system, the connection to firebase will need to be closed
