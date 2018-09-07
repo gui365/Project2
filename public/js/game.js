87c838ff60ffd91654db36e7ce78573dc716b809
Guillermo Barila <gui365@yahoo.com.ar>
2018-09-06 20:41:49 -0400

    Lose logic works

--- public/js/game.js
+++ public/js/game.js
@@ -160,7 +160,7 @@ $(document).ready(function () {
       }
       
       
-      if (questionNumber > 1) {
+      if (questionNumber > 2) {
         var random = Math.floor(Math.random() * 5);
         if (random !== 1) {
           $(".predator").animate({
@@ -181,7 +181,8 @@ $(document).ready(function () {
         var pPosition = $(".player" + i).attr("style");
         console.log(i + ": " + pPosition);
         
-        console.log($(".player" + i).css("left"));
+        // console.log($(".player" + i).css("left"));
+        console.log(i + $(".player" + i).attr("style"));
         
         // IF THE PLAYER GOT TO THE CABIN
         if ($(".player" + i).attr("style") === "left: 80vw;") {
@@ -203,20 +204,25 @@ $(document).ready(function () {
             audio.play();
           }
         // IF THE PLAYER GETS EATEN
-        } else if ($(".predator").attr("style") !== undefined && $(".player" + i).attr("style") === $(".predator").attr("style")) {
+        } else if ($(".player" + i).attr("style") === $(".predator").attr("style") && questionNumber > 1) {
+
           if (localStorage.getItem("boardScreen")) {
             $(".player" + i).hide();
             $(".player" + i).css("left", "-100vw;");
+            
+            if ($(".player1").is(":hidden") && $(".player2").is(":hidden") && $(".player3").is(":hidden")) {
+              endGame();
+            }
+
           } else if (localStorage.getItem("controller")) {
             var audio = new Audio("/sounds/game_lose.mp3");
             audio.play();
           }
         }
       }
+      console.log("Predator: " + $(".predator").attr("style"));
+      
 
-      // if ($(".player1").is(":hidden") && $(".player2").is(":hidden") && $(".player3").is(":hidden")) {
-      //   endGame();
-      // }
     }
 
     function resetQuestion() {
@@ -228,14 +234,13 @@ $(document).ready(function () {
       answerNow = false;
       
       // Stop the game after 13 questions if nobody won
-      // if (questionNumber === 14) {
-      //   endGame();
-      // } else {
+      if (questionNumber === 13) {
+        endGame();
+      }
+
       if (finish) {
-        console.log("If True: " + finish);
         return;
       } else {
-        console.log("If False: " + finish);
         generateQuestion();
       }
       // }
@@ -244,10 +249,10 @@ $(document).ready(function () {
 
   function endGame() {
     $("#game-over").click();
-        setTimeout(() => {
-          window.location.href = "/dashboard";
-          database.ref().child(sessionCode).delete();
-        }, 5000);
+    setTimeout(() => {
+      window.location.href = "/dashboard";
+      database.ref().child(sessionCode).remove();
+    }, 5000);
   }
 
   generateQuestion();
